3841dc155954351fd10dcf32f61edd774dd78d18
Andrei Sobolev <s_bizon@outlook.com>
2017-08-29 22:50:10 -0700

    Initial commit

---
+++ src/test/java/core/SubtractTest.java
@@ -0,0 +1,36 @@
+package core;
+
+import org.testng.annotations.*;
+
+import static org.hamcrest.MatcherAssert.*;
+
+import static org.hamcrest.Matchers.*;
+
+public class SubtractTest {
+
+       @Test
+
+       public void subtract_test_instanceOf() {
+
+             assertThat(Calculator.subtract(2, 2), instanceOf(Double.class));}
+
+       @Test
+
+       public void subtract_test_2_param() {
+
+              assertThat(Calculator.subtract(2, 2), equalTo(0.0));}
+
+       @Test
+
+       public void subtract_test_3_param() {
+
+    	   assertThat(Calculator.subtract(2, 2, 2 ), equalTo(-2.0));}
+
+       @Test
+
+       public void subtract_test_4_param() {
+
+              assertThat(Calculator.subtract(2, 2, 2, 2), equalTo(-4.0));}
+
+}
+ 
